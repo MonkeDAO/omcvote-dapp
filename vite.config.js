ed67b9aec3254f9203531136b3b5b2c7f57d32cd
thedonmon <22967202+thedonmon@users.noreply.github.com>
2023-04-04 19:28:54 -0400

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import inject from '@rollup/plugin-inject';
-import nodePolyfills from "rollup-plugin-node-polyfills";
+import nodePolyfills from 'rollup-plugin-node-polyfills';
 import path from 'path';
 
 /** @type {import('vite').UserConfig} */
@@ -13,36 +13,28 @@ const config = {
 		'process.env.VERSION': JSON.stringify(process.env.npm_package_version)
 	},
 
-    optimizeDeps: {
-        esbuildOptions: {
-            target: "es2020",
-        },
-    },
+	optimizeDeps: {
+		include: ['@solana/web3.js', 'buffer'],
+		esbuildOptions: {
+			target: 'es2020',
+			plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })]
+		}
+	},
 	build: {
-        target: "es2020",
-    },
-	// optimizeDeps: {
-	// 	include: ['@solana/web3.js', 'buffer'],
-	// 	esbuildOptions: {
-	// 			target: 'esnext',
-	// 			plugins: [NodeGlobalsPolyfillPlugin({ buffer: true })],
-	// 	},
-	// },
+		target: 'es2020',
+		commonjsOptions: {
+			transformMixedEsModules: true
+		},
+		rollupOptions: {
+			plugins: [inject({ Buffer: ['buffer', 'Buffer'] }), nodePolyfills({ crypto: true })]
+		}
+	},
 	resolve: {
 		alias: {
 			$stores: path.resolve('./src/stores'),
-			//stream: 'rollup-plugin-node-polyfills/polyfills/stream',
+			stream: 'rollup-plugin-node-polyfills/polyfills/stream'
 		}
 	},
-	// build: {
-	// 	target: 'esnext',
-	// 	commonjsOptions: {
-	// 		transformMixedEsModules: true
-	// 	},
-	// 	rollupOptions: {
-	// 		plugins: [inject({ Buffer: ['buffer', 'Buffer'] }), nodePolyfills({ crypto: true })],
-	// 	},
-	// },
 	server: {
 		host: true
 	}
